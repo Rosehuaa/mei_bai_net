98e9c4ae464d4e6e9a55d3018791a26683a212cf
Rose <916820048@qq.com>
2018-10-11 19:34:34 +0800

    1

---
+++ app/src/main/java/com/example/shan/week3/RateListActivity.java
@@ -0,0 +1,14 @@
+package com.example.shan.week3;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class RateListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_list);
+    }
+
+}
